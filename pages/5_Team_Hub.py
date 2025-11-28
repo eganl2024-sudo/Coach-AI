f5ba0696ce79c24ce32780f4bc4cf2b1f2f708ec
eganl2024-sudo <eganl2024@gmail.com>
2025-11-28 12:20:21 -0500

    Fix st.switch_page() - use relative path format for page navigation

--- pages/5_Team_Hub.py
+++ pages/5_Team_Hub.py
@@ -474,7 +474,8 @@ else:
                 st.session_state.generator_default_duration = inferred_duration
 
                 # Navigate to Practice Generator
-                st.switch_page("2_⚽_Practice_Generator")
+                # Use the internal navigation mechanism available in Streamlit
+                st.switch_page("pages/2_Practice_Generator.py")
 
 formation_options = ["4-3-3", "4-4-2", "3-5-2", "4-2-3-1", "4-1-4-1", "Custom"]
 raw_formation = team_row.get('formation', '')
